fa7f8d2da7f4e34b7ca22ebb25fdaca45ac03ce9
kan51 <kandai.oiso@gmail.com>
2025-10-06 21:12:49 +0900

    commit

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@
 # ライブラリの読み込み
 ############################################################
 from langchain_community.document_loaders import PyMuPDFLoader, Docx2txtLoader, TextLoader
-from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders import CSVLoader
 
 
 ############################################################
